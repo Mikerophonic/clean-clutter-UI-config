0e99ae6c5595fd09f406c832202130812df3b0f8
witherScript <chandlerscott123@gmail.com>
2023-08-20 13:05:06 -0500

    fix overlay bug, squash click listeners

--- src/animate-item.js
+++ src/animate-item.js
@@ -4,12 +4,16 @@ export default class ApplyAnimation{
   constructor(elementId){
     this.toAnimate = document.getElementById(elementId);
     this.elementId=elementId;
+    console.log('animated ', elementId);
     this.elemAdded = [];
   }
+
+  //animateItems
   toBox(){
-    this.toAnimate.addEventListener('click', ()=>{
-      gsap.to(`#${this.elementId}`, {x: -300, duration: .5, ease: 'power2.inOut'});
-      gsap.to(`#${this.elementId}`, { opacity: 0 });
-    })
+
+    //click event listener
+    const timeline = gsap.timeline();
+    timeline.to(`#${this.elementId}`, { x: -300, duration: 0.5, ease: 'power2.inOut' })
+            .to(`#${this.elementId}`, { opacity: 0, duration: 0.5 });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
-
 import "./styles.css";
-import ToolTip from "./tooltip";
-import BoxMethod from './four-box.js'
-import ApplyAnimation from "./animate-item.js";
+import BoxMethod from './four-box.js';
 
 window.addEventListener("load", function () {
-  //start trash
-
+  // Start the process with the kitchen
   const box = new BoxMethod("box");
-  box.populateItems("kitchen");
-//need: populateItems bedroom
-
-  const kitchenArr = ["cans", "pot", "forks", "winter"];
-  formatAll(kitchenArr);
+  box.populateItem("kitchen");
+  
 });
-
-function formatAll(elemArr){
-  elemArr.forEach((elem)=>{
-    mouseOverFormat(elem);
-  })
-}
-
-
-function mouseOverFormat(elementId) {
-  const animate = new ApplyAnimation(elementId);
-  animate.toBox();
-  const elemToolTip = new ToolTip(elementId);
-    document
-      .getElementById(elementId)
-      .addEventListener("mouseover", function(){
-        elemToolTip.add();
-      });
-
-    document
-      .getElementById(elementId)
-      .addEventListener("mouseleave", function () {
-        elemToolTip.remove();
-      });
-}
