50ca1d35e8cb367641f91708107cdc6d08ba67ee
witherScript <chandlerscott123@gmail.com>
2023-08-20 01:47:53 -0500

    working game logic for kitchen minus finish. need: abstraction for other rooms

--- src/animate-item.js
+++ src/animate-item.js
@@ -4,14 +4,12 @@ export default class ApplyAnimation{
   constructor(elementId){
     this.toAnimate = document.getElementById(elementId);
     this.elementId=elementId;
+    this.elemAdded = [];
   }
   toBox(){
     this.toAnimate.addEventListener('click', ()=>{
       gsap.to(`#${this.elementId}`, {x: -300, duration: .5, ease: 'power2.inOut'});
-      gsap.to(`#${this.elementId}`, { opacity: 0 })
-.set(`#${this.elementId}`, { backgroundColor: "red", innerText: "End the test." })
-.to(`#${this.elementId}`, { opacity: 1 }, "+=1");
-
+      gsap.to(`#${this.elementId}`, { opacity: 0 });
     })
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import BoxMethod from './four-box.js'
 import ApplyAnimation from "./animate-item.js";
 
 window.addEventListener("load", function () {
-  mouseOverFormat("cans");
-  mouseOverFormat("pot");
-  mouseOverFormat("forks");
-  mouseOverFormat('winter');
-  setOnStart();
+  //start trash
+
+  const box = new BoxMethod("box");
+  box.populateItems("kitchen");
+//need: populateItems bedroom
+
+  const kitchenArr = ["cans", "pot", "forks", "winter"];
+  formatAll(kitchenArr);
 });
 
-function setOnStart(){
-  const fourbox = new BoxMethod('box');
-  return fourbox;
+function formatAll(elemArr){
+  elemArr.forEach((elem)=>{
+    mouseOverFormat(elem);
+  })
 }
+
 
 function mouseOverFormat(elementId) {
   const animate = new ApplyAnimation(elementId);
